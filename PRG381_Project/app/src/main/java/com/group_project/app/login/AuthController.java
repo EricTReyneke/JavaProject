490294cc65af3fc168669b2042a148fd818af081
Arie <576742@student.belgiumcampus.ac.za>
2022-08-28 18:55:27 +0200

    routes

--- PRG381_Project/app/src/main/java/com/group_project/app/login/AuthController.java
+++ PRG381_Project/app/src/main/java/com/group_project/app/login/AuthController.java
@@ -21,22 +21,22 @@ public class AuthController {
     }
 
     // handler method to handle home page request
-    @GetMapping("/index")
+    @GetMapping("")
     public String home(){
-        return "index";
+        return "Login";
     }
 
-    // handler method to handle user registration form request
-    @GetMapping("/register")
+    // method to handle user registration form request
+    @GetMapping("/Details")
     public String showRegistrationForm(Model model){
         // create model object to store form data
         UserDto user = new UserDto();
         model.addAttribute("user", user);
-        return "register";
+        return "Details";
     }
 
-    // handler method to handle user registration form submit request
-    @PostMapping("/register/save")
+    // method to handle user registration form submit request
+    @PostMapping("/Details/save")
     public String registration(@Valid @ModelAttribute("user") UserDto userDto,
                                BindingResult result,
                                Model model){
@@ -44,12 +44,12 @@ public class AuthController {
 
         if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
             result.rejectValue("email", null,
-                    "There is already an account registered with the same email");
+                    "There is already an account");
         }
 
         if(result.hasErrors()){
             model.addAttribute("user", userDto);
-            return "/register";
+            return "/Details";
         }
 
         userService.saveUser(userDto);
